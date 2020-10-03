10fc25c3ded42f1943604eec4eb0389905c0bf0c
khamidn <nurkhamid014@gmail.com>
2020-10-03 18:36:27 +0700

    add model ProductGallery

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,8 +17,8 @@ class Product extends Model
 
     ];
 
-    // public function galleries()
-    // {
-    // 	return $this->hasMany(ProductGallery::class, 'product_id');
-    // }
+    public function galleries()
+    {
+    	return $this->hasMany(ProductGallery::class, 'product_id');
+    }
 }
